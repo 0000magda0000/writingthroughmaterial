7aae1995513fc8f908f56b77edf742e135d21392
Magda Barthofer <magda.barthofer@gmail.com>
2020-06-08 22:29:57 +0200

    deleted forgotten lines in writing model

--- app/models/writing.rb
+++ app/models/writing.rb
@@ -1,13 +1,3 @@
 class Writing < ApplicationRecord
   has_one_attached :document
-  validate :mp3
-
-  def mp3
-  return unless writings.attached?
-  end
-
-  acceptable_types = ["audio/mp3", "attachment/pdf", "image/jpeg", "image/png"]
-    unless acceptable_types.include?(writing.content_type)
-      errors.add(:writing, "must be a PDF, JPEG, PNG or MP3")
-    end
 end
